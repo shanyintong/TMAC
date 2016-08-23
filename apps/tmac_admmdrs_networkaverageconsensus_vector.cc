55cc6301abed7138f19b80ffd8557b7430bf8fe9
LuLingxi <594507@163.com>
2016-08-23 12:43:14 -0700

    add lock to network average consensus add the app of the vector ver. network average consensus

--- apps/tmac_admmdrs_networkaverageconsensus_vector.cc
+++ apps/tmac_admmdrs_networkaverageconsensus_vector.cc
@@ -1,5 +1,5 @@
 /**********************************
- * Example 1: solving Network Average Consensus
+ * Example 1: solving Network Average Consensus, which the basic units are vectors
  * 
  *    min_x sum_{i =1}^N ||x - theta_i||^2
  *
@@ -17,17 +17,21 @@
 #include "tmac.h"
 #include "util.h"
 #include <thread>
-#include <mutex>          // std::mutex
 using namespace std;
 //using namespace MyAlgebra;
 #include "algebra_namespace_switcher.h"
+#include <mutex>          // std::mutex
 
-double objective(Vector theta_, double& avrg, double& weight) ;
+
+void get_result(Vector* avrg, Vector* results, double weight, int unit_size);
+mutex lock_of_avrg;
 
 int main(int argc, char *argv[]) {
     
-  mutex lock_of_avrg;
-
+  int unit_size = 500;
+  Vector results;
+  results.resize(unit_size);
+   
   // Step 0: Define the parameters and input file names  
   Params params;
   //string data_file_name;
@@ -48,69 +52,68 @@ int main(int argc, char *argv[]) {
   //params.problem_size = problem_size;
   int problem_size = params.problem_size;
   params.tmac_step_size = 0.01;
-  params.max_itrs = 1000;
+  params.max_itrs = 10000;
   params.worker_type = "gs" ;
-  //params.async = false;
-  Vector theta_(problem_size, 3.);
-  for(int i = 0; i < problem_size; i++)
-    theta_[i] = i + 1;   
-  Vector x(problem_size, 0.);   // unknown variables, initialized to zero
-  double avrg = 0. ;// maintained variables, initialized to zero
+  double weight_gamma = 1.;
+    
+  vector<Vector> theta_;
+  for(int i = 0; i < problem_size; ++i){
+    Vector theta_i(unit_size, (double)i+1);
+    for(int j = 0; j < unit_size; ++j)
+        theta_i[j] += j;
+    theta_.push_back(theta_i);
+  }
+
+  vector<Vector> x;// unknown variables, initialized to zero
+  for(int i = 0; i < problem_size; ++i){
+    Vector x_i(unit_size, 0.);
+    x.push_back(x_i);
+  }
+
+  Vector avrg(unit_size, 0.);// maintained variables, initialized to zero
+
   // Step 3. Define your three operators based on data and parameters
   double second_operator_step_size = 0.01;
   params.step_size = second_operator_step_size;
-  double weight_gamma = 1.;
-  op2_for_network_average_consensus<Vector> op2(&theta_, &avrg, second_operator_step_size, weight_gamma);  
-  using First = decltype(op2);
+
+  op2_for_network_average_consensus_vector<vector<Vector>> op2(&theta_, &avrg, second_operator_step_size, weight_gamma);
+  using Second = decltype(op2);
   double first_operator_step_size = 0.01;
   params.step_size = first_operator_step_size;
-
-  op1_for_network_average_consensus<Vector> op1(&theta_, first_operator_step_size, weight_gamma);  
-  using Second = decltype(op1);
+  
+  op1_for_network_average_consensus_vector<vector<Vector>> op1(&theta_, first_operator_step_size, weight_gamma);
+  using First = decltype(op1);
 
   double third_operator_step_size = 0.01;
   params.step_size = third_operator_step_size;
 
-  op3_for_network_average_consensus<Vector> op3(&theta_, &avrg, &lock_of_avrg, third_operator_step_size, weight_gamma);
+  op3_for_network_average_consensus_vector<vector<Vector>> op3(&theta_, &avrg, &lock_of_avrg, third_operator_step_size, weight_gamma);
   using Third = decltype(op3);
-
+   
   // Step 4. Define your operator splitting scheme
-  DoglasRachfordSplittingAdmm<op1_for_network_average_consensus<Vector>, 
-    op2_for_network_average_consensus<Vector>, op3_for_network_average_consensus<Vector> > 
+  DoglasRachfordSplittingAdmm_vector<op1_for_network_average_consensus_vector<vector<Vector>> ,
+        op2_for_network_average_consensus_vector<vector<Vector>>, op3_for_network_average_consensus_vector<vector<Vector>>> 
     DRs(&x, op1, op2, op3);
 
   // Step 6. Call the TMAC function
-  double start_time = get_wall_time();  
+  double start_time = get_wall_time();
   TMAC(DRs, params);
   double end_time = get_wall_time();  
 
   print_parameters(params);
 
-    
-  double real_avrg;
-    for(int t = 0; t < problem_size; ++t){
-     // cout<<"x_" << t << " = "<< x[t] <<' ';
-      real_avrg += x[t];
-    }
-  real_avrg /= problem_size;
-  cout<<endl;
-  cout<<"avrg = "<< avrg <<endl;
-  cout<<"real avrg = "<<real_avrg<<endl;
-    
   cout << "Computing time is: " << end_time - start_time << endl;  
   // Step 7. Print results
-
-  cout << "Objective value is: " << objective(theta_, avrg, weight_gamma) << endl;
+  get_result(&avrg, &results, weight_gamma, unit_size);
+  //cout << "Objective value is: " << objective(theta_, avrg, weight_gamma) << endl;
+  print(results);
   cout << "---------------------------------" << endl;  
   return 0;
 }
 
 
-double objective(Vector theta_, double& avrg, double& weight) {
-  int len = theta_.size();
-  double y = -avrg / weight;
-//  double result = 0.;
-//  for(int i = 1; i <= len; ++i)
-//    result += ((theta_)[i] - y) * ((theta_)[i] - y);
-  return y;
+void get_result(Vector* avrg, Vector* results, double weight, int unit_size){
+  //int len = theta_.size();
+  copy(*avrg, *results, 0, unit_size);
+  scale(*results, -1./(double)weight);
 }
